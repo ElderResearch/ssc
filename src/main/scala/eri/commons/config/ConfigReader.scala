20eebc78519c124320131fd448f4fe2738e09bba
Simeon H.K. Fitch <fitch@datamininglab.com>
2016-05-15 10:20:41 -0400

    Initial conversion to typeclass-based conversion.

---
+++ src/main/scala/eri/commons/config/ConfigReader.scala
@@ -0,0 +1,54 @@
+package eri.commons.config
+
+import java.io.File
+import java.nio.file.{Paths, Path}
+import java.time.Duration
+
+import com.typesafe.config.Config
+
+/**
+ * Typeclass specification and default implementations for reading a specific type from a `Config`
+ *
+ * @author <a href="mailto:fitch@datamininglab.com">Simeon H.K.Fitch</a>
+ * @since 5/15/16
+ */
+trait ConfigReader[T] {
+  def apply(path: String, config: Config): T
+}
+
+object ConfigReader {
+  implicit object BooleanReader extends ConfigReader[Boolean] {
+    override def apply(path: String, config: Config): Boolean = config.getBoolean(path)
+  }
+  implicit object IntReader extends ConfigReader[Int] {
+    override def apply(path: String, config: Config): Int = config.getInt(path)
+  }
+  implicit object DoubleReader extends ConfigReader[Double] {
+    override def apply(path: String, config: Config): Double = config.getDouble(path)
+  }
+  implicit object LongReader extends ConfigReader[Long] {
+    override def apply(path: String, config: Config): Long = config.getLong(path)
+  }
+  implicit object FloatReader extends ConfigReader[Float] {
+    override def apply(path: String, config: Config): Float = config.getDouble(path).toFloat
+  }
+  implicit object StringReader extends ConfigReader[String] {
+    override def apply(path: String, config: Config): String = config.getString(path)
+  }
+  implicit object DurationReader extends ConfigReader[Duration] {
+    override def apply(path: String, config: Config): Duration = config.getDuration(path)
+  }
+  implicit object PathReader extends ConfigReader[Path] {
+    override def apply(path: String, config: Config): Path = Paths.get(config.getString(path))
+  }
+  implicit object FileReader extends ConfigReader[File] {
+    override def apply(path: String, config: Config): File = PathReader(path, config).toFile
+  }
+  implicit object ConfigReader extends ConfigReader[Config] {
+    override def apply(path: String, config: Config): Config = config.getConfig(path)
+  }
+  implicit object AnyRefReader extends ConfigReader[AnyRef] {
+    override def apply(path: String, config: Config): AnyRef = config.getObject(path)
+  }
+
+}
